a1aa882601e44d3b0a3678adf376ad50f65f9ab8
nicobelen <nicolasbelen93@gmail.com>
2022-02-03 10:22:23 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %zu bytes\n", sizeof(charType));
 	printf("Size of a int: %zu bytes\n", sizeof(intType));
-	printf("Size of a long: %zu bytes\n", sizeof(longType));
-	printf("Size of a long long: %zu bytes\n", sizeof(longlongType));
+	printf("Size of a long int: %zu bytes\n", sizeof(longType));
+	printf("Size of a long long int: %zu bytes\n", sizeof(longlongType));
 	printf("Size of a float: %zu bytes\n", sizeof(floatType));
 	return (0);
 }
