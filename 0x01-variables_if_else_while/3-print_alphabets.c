0726722a48ccab74a965cdb988e56b8d10af5b05
nicobelen <nicolasbelen93@gmail.com>
2022-02-04 08:13:21 -0800

    lowandup

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,14 @@ int main(void)
 	int x;
 
 		for (x = 'a'; x <= 'z'; x++)
+		{
 			putchar(x);
 			putchar('\n');
+		}
 		for (x = 'A'; x <= 'Z'; x++)
+		{
 			putchar(x);
 			putchar('\n');
+		}
 		return (0);
 }
