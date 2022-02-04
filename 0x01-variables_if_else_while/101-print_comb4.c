d02da3a6f048493f9c18792f8888238b297cf2ee
nicobelen <nicolasbelen93@gmail.com>
2022-02-04 12:25:22 -0800

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,23 +10,22 @@ int main(void)
 	int i, j, k;
 
 	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = (i + 1); j <= '8'; j++)
 		{
-			for (j = (i + 1); j <= '8'; j++)
+			for (k = (j + 1); k <= '9'; k++)
 			{
-				for (k = (j + 1); k <= '9'; k++)
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < '7')
 				{
-						putchar(i);
-						putchar(j);
-						putchar(k);
-
-					if (i < '7' || j < '8' || k < '9')
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
