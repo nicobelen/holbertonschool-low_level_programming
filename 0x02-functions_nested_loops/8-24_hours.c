c151fe4ca98f0b3f5bf63f25a9ad1a35050b66d4
nicobelen <nicolasbelen93@gmail.com>
2022-02-07 10:34:22 -0800

    commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* jack_bauer - code
+* Return: Always 0
+*
+*
+*/
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = (i + 1); j <= 59; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+
+		if (i < 98)
+		{
+			_putchar('\n');
+		}
+	
+		}
+	}
+}
