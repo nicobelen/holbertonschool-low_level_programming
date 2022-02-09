ff70ebbee68146caf1a8035953f8293858a2e0a4
nicobelen <nicolasbelen93@gmail.com>
2022-02-09 11:14:30 -0800

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
-*
-*
-*
+* _isupper - code
+* @c: parameter
+* Return: something
 *
 */
 
 int _isupper(int c);
 {
-	if (c = isupper (c))
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
+	}
+	else
+	{
+		c = (97 - 122)
 	}
 
 	return (0);
