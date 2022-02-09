e168a24157c134b0bb44c2e7cb0eca64770980ae
nicobelen <nicolasbelen93@gmail.com>
2022-02-09 12:19:45 -0800

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-*
-*
+* print_most_numers - code
+* Return: something
 *
 */
 void print_most_numbers(void)
 {
-	int a, b;
+	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		if (a != 2 && a != 4)
+		if (a != '2' && a != '4')
 			_putchar(a);
 	}
 
