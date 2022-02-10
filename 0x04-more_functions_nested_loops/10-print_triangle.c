688471ddc41547183e6a20322cc2ee7aa3b35463
nicobelen <nicolasbelen93@gmail.com>
2022-02-09 18:37:38 -0800

    commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,13 +10,13 @@ void print_triangle(int size)
 	int x = 0;
 	int y = 0;
 
-	for (x = 1; x <= size; x++)
+	for (x = 0; x < size; x++)
 	{
 		for (y = x; y < size; y++)
 		{
 			_putchar(' ');
 		}
-		for (y = 1; y <= x; y++)
+		for (y = 0; y <= x; y++)
 		{
 			_putchar('#');
 		}
