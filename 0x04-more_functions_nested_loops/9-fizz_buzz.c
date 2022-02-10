1231af40be61fda3c59aff116439e4e48c177411
nicobelen <nicolasbelen93@gmail.com>
2022-02-09 17:55:03 -0800

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,18 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("fizzbuzz");
-		if (i % 3 == 0)
+		
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+
+		else if (i % 3 == 0)
 			printf("Fizz ");
-		if (i % 5 == 0)
+
+		else if (i % 5 == 0)
 			printf("Buzz ");
-		if ((i % 3 != 0) && (i % 5 != 0))
-		printf("%d ", i);
+
+		else
+			printf("%d ", i);
 	}
 
 	printf("\n");
