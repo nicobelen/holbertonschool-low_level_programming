9189b4569c115c7e74fde3b7a3487c4c68a91370
nicobelen <nicolasbelen93@gmail.com>
2022-02-11 11:55:07 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* _strlen - code
+* @s: parameter
+* Return: reverse string
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char temp;
+
+
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+
+		i++;
+		j--;
+	}
+}
