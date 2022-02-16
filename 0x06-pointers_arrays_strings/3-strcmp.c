997c5acdf8dfa631db036903238409dd745f0127
nicobelen <nicolasbelen93@gmail.com>
2022-02-16 06:03:52 -0800

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,23 +8,10 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-    int res;
-
-    res = *s1 - *s2;
-
-    while (s1[i] == s2[i] && s1[i] != '\0')
+    while (*s1 == *s2)
 	{
-        i++;
-
-		if (s1[i] < s2[i])
-			return (res);
-
-		else if (s1[i] > s2[i])
-			return (res);
-
-		else
-			return (0);
+        s1++;
+		s2++;
 	}
-	return (res);
+	return (*s1 - *s2);
 }
