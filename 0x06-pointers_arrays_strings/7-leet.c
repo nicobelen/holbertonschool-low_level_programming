874c9e4112b5629516b842e5bcb10f450a2b51c9
nicobelen <nicolasbelen93@gmail.com>
2022-02-16 10:10:17 -0800

    commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,11 +8,18 @@
 */
 char *leet(char *s)
 {
+	char *l = "aAeEoOtTlL";
+	char *n = "4433007711";
 	int i;
+	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) == 97) || (*(s + i) == 65)
-			*(s + i) == )
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == l[j])
+				s[i] = n[j];
+		}
 	}
+	return (s);
 }
