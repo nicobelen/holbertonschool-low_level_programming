215e74616aa895c742aa4f95919faebdc58701a6
nicobelen <nicolasbelen93@gmail.com>
2022-02-21 08:28:16 -0800

    commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strspn - prefix substring
+* @s: pointer
+* @accept: pointer
+* Return: k
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+				{
+					k++;
+				}
+			}
+		}
+		else
+		{
+			return (k);
+		}
+	}
+	return (k);
+}
