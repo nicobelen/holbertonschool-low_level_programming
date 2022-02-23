ab0e059ae22a9bac7bb4375e035474b00939af90
nicobelen <nicolasbelen93@gmail.com>
2022-02-23 10:31:53 -0800

    commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* square_root - check code
+* _sqrt_recursion - check code
+* @n: parameter
+* @root: parameter
+* Return: 0
+*/
+int square_root(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+
+	if (root * root == n)
+		return (root);
+
+	return (square_root(n, root + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (square_root(n, 0));
+}
