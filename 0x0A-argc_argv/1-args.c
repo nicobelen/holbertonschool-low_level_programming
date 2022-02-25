c13930de277d2381421af4033ca1f8c61f8479b5
nicobelen <nicolasbelen93@gmail.com>
2022-02-25 08:42:23 -0800

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*
+*
+*
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
