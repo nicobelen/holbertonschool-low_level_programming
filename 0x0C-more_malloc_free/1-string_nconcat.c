49a2739bcb7ace9b61d226e68b7ac5a295e8c76b
nicobelen <nicolasbelen93@gmail.com>
2022-03-03 10:40:56 -0800

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,32 +11,35 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ret;
-	unsigned int i, j, str1, str2;
+		char *ret;
+        unsigned int len = n, i;
 
-	if (s1 == NULL)
-		s1 = "";
+		if (s1 == NULL)
+			s1 = "";
 
-	if (s2 == NULL)
-		s2 = "";
+		if (s2 == NULL)
+			s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; j < n; j++)
-		;
+		for (i = 0; s1[i]; i++)
+			len++;
 
-	ret = malloc(i * sizeof(char) * n + 1);
+		ret = malloc(sizeof(char) * (len + 1));
 
-	if (ret == NULL)
-		return (NULL);
+		if (ret == NULL)
+			return (NULL);
 
-	for (str1 = 0, str2 = 0; str1 < i + j; str1++)
-	{
-		if (str1 < i)
-			ret[str1] = s1[str1];
-		else
-			ret[str1] = s2[str2++];
-	}
+		len = 0;
 
-	return (ret);
+		for (i = 0; s1[i]; i++)
+		{
+			ret[len++] = s1[i];
+		}
+
+		for (i = 0; s2[i] && i < n; i++)
+		{
+			ret[len++] = s2[i];
+			ret[len] = '\0';
+		}
+
+		return (ret);
 }
