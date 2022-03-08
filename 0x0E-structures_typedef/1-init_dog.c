62cb24e967f48a6eb2f5592f9c91ac32f24aefa9
nicobelen <nicolasbelen93@gmail.com>
2022-03-08 12:07:33 -0800

    init dog betty

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,11 @@
 #include "dog.h"
 
 /**
-*
-*
-*
-*
+* init_dog - initialize struct dog
+* @d: parameter
+* @name: parameter
+* @age: parameter
+* @owner: parameter
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
