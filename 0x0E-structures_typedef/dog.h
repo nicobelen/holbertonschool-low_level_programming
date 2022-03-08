ccb7804e7b5a527e0ea5fd89aeaf36cbcc93656d
nicobelen <nicolasbelen93@gmail.com>
2022-03-08 11:37:03 -0800

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct dog - structure
+* @name: data
+* @age: data
+* @owner: data
+*/
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
