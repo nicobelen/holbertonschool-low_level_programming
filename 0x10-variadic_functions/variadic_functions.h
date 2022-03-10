709ebd5653c25bd541727cb921411075335bd2fc
nicobelen <nicolasbelen93@gmail.com>
2022-03-10 08:35:44 -0800

    print string

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif
