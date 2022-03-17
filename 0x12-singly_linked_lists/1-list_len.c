0d5b5a607914f6c41102b178416775a6912201b6
nicobelen <nicolasbelen93@gmail.com>
2022-03-17 11:40:47 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* list_len - print elements of a list
+* @h: head of list
+* Return: counter.
+*/
+size_t list_len(const list_t *h)
+{
+    unsigned int c = 0;
+
+    while (h != NULL)
+    {
+        h = h->next;
+        c++;
+    }
+    return (c);
+}
