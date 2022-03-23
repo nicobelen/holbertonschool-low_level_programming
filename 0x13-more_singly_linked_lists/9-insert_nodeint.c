a40fb0f6316d27de149555cef6c23b450bf4ed7e
nicobelen <nicolasbelen93@gmail.com>
2022-03-22 18:47:44 -0700

    insert node at given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,6 +4,7 @@
 * insert_nodeint_at_index - check code
 * @head: pointer to head of list
 * @idx: index where the node should be added
+* @n: given position
 * Return: adress of newnode
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -29,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (idx > 1)
 	{
 		tmp = tmp->next;
-		idx++;
+		idx--;
 
 		if (tmp == NULL)
 		{
