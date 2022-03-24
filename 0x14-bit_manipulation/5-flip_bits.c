3072cc3cefb66dc1536d0e1c6e4f116a16f01553
nicobelen <nicolasbelen93@gmail.com>
2022-03-24 13:48:21 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* countbits - recursive counter function
+* @n: int n
+* Return: counter
+*/
+int countbits(int n)
+{
+	int count = 0;
+
+	while (n > 0)
+	{
+		count++;
+		n &= (n - 1);
+	}
+
+	return (count);
+}
+/**
+* flip_bits - return number of bits to be flipped
+* @n: number
+* @m: bits
+* Return: result of countbits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (countbits(n ^ m));
+}
