f8fe648aeddf68c2f945b29d99e0c62591d3c6d3
nicobelen <nicolasbelen93@gmail.com>
2022-03-24 13:24:18 -0700

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ set_bit - return value of a bit at a given index
+* @n: number
+* @index: position
+* Return: value of a bit
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit = 1;
+    int clearbit = ~(1 << index);
+	int mask = *n & clearbit;
+	*n = mask | (bit << index);
+
+	if (n == 0)
+		return (-1);
+
+	if (index > 31)
+		return (-1);
+
+	return (*n);
+}
