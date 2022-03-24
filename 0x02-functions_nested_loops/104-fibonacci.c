34f5bf25f6adc6b5604f037d56ffddbdf29fb15c
nicobelen <nicolasbelen93@gmail.com>
2022-03-23 17:49:00 -0700

    first 100 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,22 @@
 */
 int main(void)
 {
-    int i;
-    unsigned long int n1 = 0, n2 = 1, n3;
+	int i;
+	unsigned long int n1 = 0, n2 = 1, n3;
 
-    for (i = 0; i < 100; i++)
-    {
-        n3 = n1 + n2;
+	for (i = 0; i < 100; i++)
+	{
+		n3 = n1 + n2;
 
-        printf("%ld", n3);
+		printf("%ld", n3);
 
-        n1 = n2;
-        n2 = n3;
+		n1 = n2;
+		n2 = n3;
 
-        if (i == 99)
-            printf("\n");
-        else
-            printf(", ");
-    }
-    return (0);
+		if (i == 99)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
 }
