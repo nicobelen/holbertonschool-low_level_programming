47ef003fbef1a30ae84d3627555622c942cd8b3d
nicobelen <nicolasbelen93@gmail.com>
2022-03-24 09:05:04 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,9 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
+	while (b[i] != '0' && b[i] != '1')
+		return (0);
+
 	while (b[i] == '0' || b[i] == '1')
 	{
 		dec <<= 1;
