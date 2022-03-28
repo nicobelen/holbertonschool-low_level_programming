0ba12d0bd4995250e1414a355c1b072839c98ef4
nicobelen <nicolasbelen93@gmail.com>
2022-03-28 16:18:50 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,17 +10,18 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int n, _write;
 
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-
 	if (text_content == NULL)
 	{
 		return (-1);
 	}
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
 	n = open(filename, O_WRONLY | O_APPEND, 0200);
+
 	if (n == -1)
 		return (-1);
 
