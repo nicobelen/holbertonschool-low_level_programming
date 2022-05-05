a5daa21f305919d33c58f12136cc62f816e49007
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-05 16:12:00 -0300

    dlisint_len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - get len of list
+ * @h: head of list
+ * Return: c
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    int c = 0;
+
+	while (h != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
