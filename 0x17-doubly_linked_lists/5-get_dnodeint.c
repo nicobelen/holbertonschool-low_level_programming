aeb9ef789dcb3a30317f86ba09ca0c430bb4b9f8
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-05 19:31:28 -0300

    5 get dnode int

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - returns nth node of a list
+* @head: pointer to head of list
+* @index: index of the node starting at 0
+* Return: nth node
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *tmp;
+
+	tmp = head;
+
+	if (tmp == NULL)
+		return (NULL);
+
+	while (tmp != NULL)
+	{
+		if (count == index)
+			return (tmp);
+		count++;
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
