f0c5d8284b3cd54938c956d64a22c74d85a37034
nicobelen <nicolasbelen93@gmail.com>
2022-05-13 06:41:16 -0700

    operations

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add - asas
+ *@b: asd
+ *@a: sdf
+ * Return:asd
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*sub - returns the difference of a and b
+*@a: aaa
+*@b: bbb
+*
+*Return: difference
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+*mul - returns the product of a and b
+*@a: aaa
+*@b: bbb
+*
+*Return: multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+*_div - returns the result of the division of a by b
+*@a: aaa
+*@b: bbb
+*
+*Return: division
+*/
+int _div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+*mod - returns the remainder of the division of a by b
+*@a: aaa
+*@b: bbb
+*
+*Return: reminder of the division
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
