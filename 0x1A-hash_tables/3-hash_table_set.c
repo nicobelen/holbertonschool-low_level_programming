b9346a37d8f623f7fccdef173003412ce6891277
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-27 12:40:25 -0300

    more corrections on 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	for (; ht->array[iter] <= ht->array[index]; iter++)
 	{
-		if (strcmp(key, ht->array[index]->key) == 0)
+		if (strcmp(ht->array[iter]->key, ht->array[index]->key) == 0)
 		{
 			ht->array[index]->value = auxvalue;
 			return (1);
