cc0d70c8d883a38f26955a374138ec81bb43e552
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-27 16:26:06 -0300

    changes on 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
 
-	hash_table_t *hashtable = malloc(sizeof(hash_table_t) * 1);
+	hash_table_t *hashtable = malloc(sizeof(hash_table_t));
 
 	if (hashtable == NULL)
 		return (NULL);
-	
-	hashtable->array = malloc(sizeof(hashtable->array) * size);
+
+	hashtable->size = size;
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (hashtable->array == NULL)
+	{
+		free(hashtable);
+		return (NULL);
+	}
 
 	for (; i < size; ++i)
 	{
