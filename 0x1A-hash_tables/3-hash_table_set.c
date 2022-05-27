b8843a5fe1e7678af41de5f77079d5a4c9f5e712
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-27 12:12:59 -0300

    correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-	ht->array[index]->key = key;
+	ht->array[index]->key = (char *) key;
 	ht->array[index]->value = auxvalue;
 
 	return (1);
