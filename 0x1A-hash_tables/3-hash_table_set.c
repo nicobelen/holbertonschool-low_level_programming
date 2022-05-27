04de273f7bb1fdcf2352c935d08c34f8552ed874
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-27 16:32:48 -0300

    betty on header

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || ht == NULL)
 		return (0);
-    index = key_index((unsigned char *)key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 	nwElm = malloc(sizeof(hash_node_t));
 	if (nwElm == NULL)
 		return (0);
