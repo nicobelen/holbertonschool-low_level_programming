ba53e5f0bc767aeab580a19aa238ab18745b1a76
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-27 11:37:46 -0300

    excercises 0 - 3

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    unsigned long int i = 0;
+
+    hash_table_t *hashtable = malloc(sizeof(hash_table_t));
+
+    hashtable->array = malloc(sizeof(hash_node_t) * size);
+
+    for (; i < size; ++i)
+    {
+        hashtable->array[i] = NULL;
+    }
+
+    return (hashtable);
+}
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht:  is the hash table you want to add or update the key/value to
+ * @key: is the key
+ * @value: is the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index;
+    char *auxvalue = strdup(value);
+
+    if(key == NULL)
+        return (0);
+
+    ht = malloc(sizeof(hash_table_t));
+
+    if(ht == NULL)
+        return (0);
+    
+    index = hash_djb2((const unsigned char*) key);
+
+    while(ht->array[index]->key != NULL)
+    {
+        if(strcmp(key, ht->array[index]->key) == 0)
+        {
+            ht->array[index]->value = auxvalue;
+            return (1);
+        }
+    }
+    ht->array[index]->key = (char*)key;
+    ht->array[index]->value = auxvalue;
+
+    return (1);
+}
