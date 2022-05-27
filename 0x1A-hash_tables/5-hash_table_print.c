1e3da15289ab4c7b4f91962b28997679ffc9cbf5
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-27 18:58:14 -0300

    corrections on 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,41 +1,43 @@
 #include "hash_tables.h"
 
 /**
- * @brief 
- * 
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *tmp = NULL;
-    unsigned long int i = 0, com = 0;
+	hash_node_t *tmp = NULL;
+	unsigned long int i = 0, com = 0;
 
-    if (ht == NULL)
-		return; 
+	if (ht == NULL)
+		return;
 
-    tmp = malloc(sizeof(hash_node_t));
+	tmp = malloc(sizeof(hash_node_t));
 
-    if (tmp == NULL)
-        return;
+	if (tmp == NULL)
+		return;
 
-    printf("{");
-    for (; i < ht->size; i++)
-    {
-        if (ht->array[i] == NULL)
-        {
-            i++;
-            continue;
-        }
-        if (com == 1)
-            printf(", ");
-        com = 1;
-        tmp = ht->array[i];
-        while (tmp)
-        {
-            printf("'%s': '%s'", tmp->key, tmp->value);
-            if (tmp->next != NULL)
-                printf(", ");
-            tmp = tmp->next;
-        }
-    }
-    printf("}\n");
+	printf("{");
+
+	for (; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+		{
+			i++;
+			continue;
+		}
+		if (com == 1)
+			printf(", ");
+		com = 1;
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			if (tmp->next != NULL)
+				printf(", ");
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
 }
