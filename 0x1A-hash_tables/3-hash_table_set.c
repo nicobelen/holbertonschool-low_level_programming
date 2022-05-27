4909b907644899c65f09a5e95c72f28ea2d9af6c
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-27 14:15:56 -0300

    3 compiled

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,14 +10,12 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index  = 0;
-	char *auxvalue = strdup(value);
-	char *auxkey = strdup(key);
 	hash_node_t *nwElm, *aux;
 
 	if (key == NULL || ht == NULL)
 		return (0);
 
-	index = key_index((unsigned char *) key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 
 	nwElm = malloc(sizeof(hash_node_t));
 
@@ -29,13 +27,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(aux->key, key) == 0)
 		{
 			free(aux->value);
-			aux->value = auxvalue;
+			aux->value = strdup(value);
 			free(nwElm);
 			return (1);
 		}
 	}
-	nwElm->key = auxkey;
-	nwElm->value = auxvalue;
+	nwElm->key = strdup(key);
+	nwElm->value = strdup(value);
 	nwElm->next = ht->array[index];
 	ht->array[index] = nwElm;
 
