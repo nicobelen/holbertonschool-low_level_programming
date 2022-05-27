7742d1c9c927518608f7babe69501e79ed6c53e0
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-27 13:20:56 -0300

    checking 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL)
 		return (0);
 
-	index = hash_djb2((const unsigned char *) key);
+	index = key_index((const unsigned char *) key, sizeof(ht));
 
 	for (; ht->array[iter] <= ht->array[index]; iter++)
 	{
