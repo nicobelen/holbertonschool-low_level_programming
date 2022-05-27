6417cdd6c7125a4ec68c7d7b25434078bbc6b5c7
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-05-27 15:08:23 -0300

    discard changes on 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,22 +8,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-    hash_node_t *aux;
 
 	hash_table_t *hashtable = malloc(sizeof(hash_table_t) * 1);
 
 	if (hashtable == NULL)
 		return (NULL);
-
-    aux = malloc(sizeof(hash_node_t));
 	
 	hashtable->array = malloc(sizeof(hashtable->array) * size);
 
-	while (hashtable->array[i] != NULL)
+	for (; i < size; ++i)
 	{
-        aux = hashtable->array[i];
-        aux = aux->next;
-		free(aux);
+		hashtable->array[i] = NULL;
 	}
 
 	return (hashtable);
