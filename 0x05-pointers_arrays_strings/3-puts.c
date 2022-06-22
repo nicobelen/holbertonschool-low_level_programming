fdd17288b77ec8cc23297a164923e2b8a7ea13ee
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-22 16:29:27 -0300

    restore 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - codigo
+ * @str : puntero
+*/
+void _puts(char *str)
+{
+	int a = 0;
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
