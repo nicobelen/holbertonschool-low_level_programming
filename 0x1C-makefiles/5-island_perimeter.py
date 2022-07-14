ca267d7f3bc02076322c786be4e4f07fe35f1329
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-07-14 18:15:18 -0300

    push

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,12 +11,13 @@ def island_perimeter(grid):
         return 0
     islands, overlaps = 0, 0
     col_num = len(grid[0])
-    for i in range(row_num):
-        for j in range(col_num):
-            if (grid[i][j] == 1):
-                islands += 1
-                if (i < row_num - 1 and grid[i + 1][j] == 1):
-                    overlaps += 1
-                if (j < col_num - 1 and grid[i][j + 1] == 1):
-                    overlaps += 1
-    return islands * 4 - overlaps * 2
+    if col_num <= 99:
+        for i in range(row_num):
+            for j in range(col_num):
+                if (grid[i][j] == 1):
+                    islands += 1
+                    if (i < row_num - 1 and grid[i + 1][j] == 1):
+                        overlaps += 1
+                    if (j < col_num - 1 and grid[i][j + 1] == 1):
+                        overlaps += 1
+        return islands * 4 - overlaps * 2
